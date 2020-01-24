a3704b368710b383c31cdf4105879d11c1350a24
bobimortais <robsonzacarias@hotmail.com>
2020-01-24 18:22:22 -0300

    Add swagger

--- backend/delivery/src/main/java/com/smd/delivery/controller/DeliveryController.java
+++ backend/delivery/src/main/java/com/smd/delivery/controller/DeliveryController.java
@@ -2,16 +2,31 @@ package com.smd.delivery.controller;
 
 import com.smd.delivery.entity.Delivery;
 import com.smd.delivery.entity.DeliveryList;
+import com.smd.delivery.error.ResponseError;
 import com.smd.delivery.service.DeliveryService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
+import org.apache.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.PropertySource;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Date;
+
 @RestController
+@PropertySource("classpath:messages.properties")
+@Api(value = "Delivery API", description = "API to manage deliveries")
 public class DeliveryController
 {
     private DeliveryService deliveryService;
+
+    private static final Logger logger = Logger.getLogger(DeliveryController.class);
+
+    @Value("${INTERNAL_ERROR_EXCEPTION_MESSAGE}")
+    private String EXCEPTION_ERROR_MESSAGE;
 
     @Autowired
     public DeliveryController(DeliveryService deliveryService)
@@ -21,6 +36,7 @@ public class DeliveryController
 
     @CrossOrigin
     @GetMapping("/getDeliveries")
+    @ApiOperation(value = "Endpoint to get all deliveries")
     public ResponseEntity<DeliveryList> getDeliveries()
     {
         return new ResponseEntity<>(deliveryService.getDeliveries(), HttpStatus.OK);
@@ -28,6 +44,7 @@ public class DeliveryController
 
     @CrossOrigin
     @GetMapping("/getDeliveries/{deliveryId}")
+    @ApiOperation(value = "Endpoint to get a delivery by its id")
     public ResponseEntity<Delivery> getDelivery(@PathVariable(value="deliveryId") int deliveryId)
     {
         return new ResponseEntity<>(deliveryService.getDeliveryById(deliveryId), HttpStatus.OK);
@@ -35,6 +52,7 @@ public class DeliveryController
 
     @CrossOrigin
     @PostMapping(value = "/createDelivery", consumes = {"application/json"})
+    @ApiOperation(value = "Endpoint to create a new delivery")
     public ResponseEntity<String> createDelivery(@RequestBody(required = true) String requestDelivery)
     {
         int deliveryId = deliveryService.createDelivery(requestDelivery);
@@ -43,6 +61,7 @@ public class DeliveryController
 
     @CrossOrigin
     @PutMapping(value = "/addItemToDelivery", consumes = {"application/json"})
+    @ApiOperation(value = "Endpoint to add an item to a given delivery")
     public ResponseEntity<String> addItemToDelivery(@RequestBody(required = true) String itemToAddInfo)
     {
         deliveryService.addItemToDelivery(itemToAddInfo);
@@ -51,6 +70,7 @@ public class DeliveryController
 
     @CrossOrigin
     @DeleteMapping(value = "/removeItemFromDelivery")
+    @ApiOperation(value = "Endpoint to remove an item to a given delivery")
     public ResponseEntity<String> removeItemFromDelivery(@RequestParam(value="itemId", required = true) int itemId)
     {
         deliveryService.removeItemFromDelivery(itemId);
@@ -59,6 +79,7 @@ public class DeliveryController
 
     @CrossOrigin
     @PostMapping(value = "/removeItemsFromDelivery")
+    @ApiOperation(value = "Endpoint to remove several items from a given delivery")
     public ResponseEntity<String> removeItemsFromDelivery(@RequestBody(required = true) String requestItems)
     {
         deliveryService.removeItemsFromDelivery(requestItems);
@@ -67,6 +88,7 @@ public class DeliveryController
 
     @CrossOrigin
     @DeleteMapping(value = "/deleteDelivery")
+    @ApiOperation(value = "Endpoint to get all deliveries")
     public ResponseEntity<String> deleteDelivery(@RequestParam(value="deliveryId", required = true) int deliveryId)
     {
         deliveryService.deleteDelivery(deliveryId);
@@ -75,10 +97,24 @@ public class DeliveryController
 
     @CrossOrigin
     @PutMapping(value = "/updateDelivery")
+    @ApiOperation(value = "Endpoint to update a delivery")
     public ResponseEntity<String> updateDelivery(@RequestBody(required = true)  String requestDelivery)
     {
         deliveryService.updateDelivery(requestDelivery);
         return new ResponseEntity<>(HttpStatus.OK);
+    }
+
+    /**
+     * Controller exception handler for Exception class
+     */
+    @ExceptionHandler({ Exception.class})
+    public ResponseEntity<ResponseError> handleException(Exception e)
+    {
+        ResponseError internalErrorException = new ResponseError(new Date(),
+                EXCEPTION_ERROR_MESSAGE,
+                HttpStatus.INTERNAL_SERVER_ERROR);
+        logger.error(EXCEPTION_ERROR_MESSAGE, e);
+        return new ResponseEntity<ResponseError>(internalErrorException, HttpStatus.INTERNAL_SERVER_ERROR);
     }
 }
 
